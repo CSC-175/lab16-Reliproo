1d029183ed719dab1154659c47bbccd58c5950cf
Johns Hopkins <johnshop@umich.edu>
2024-11-14 10:51:58 -0500

    push

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,27 @@
 // Code to implement the gcf function goes here
+
+double gcf(double a, double b)
+{
+    int bigger;
+    int smaller;
+    int remainder;
+    if (a > b)
+    {
+        bigger = a;
+        smaller = b;
+    }
+    else
+    {
+        bigger = b;
+        smaller = a;
+    }
+
+    remainder = bigger % smaller;
+    if (remainder == 0)
+        return smaller;
+    else
+        return gcf(smaller, remainder);
+
+
+
+}
